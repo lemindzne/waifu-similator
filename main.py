c25993db696ab51dae3417f67cf2ace8593d9284
lemindzne <gugagugu01z@gmail.com>
2026-04-20 21:50:54 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
+import discord
+import os
+from discord.ext import commands
 
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+# Tự động load các file trong thư mục cogs
+async def load_extensions():
+    for filename in os.listdir('./cogs'):
+        if filename.endswith('.py'):
+            await bot.load_extension(f'cogs.{filename[:-3]}')
+
+@bot.event
+async def on_ready():
+    print(f'Đã đăng nhập thành: {bot.user}')
+    await load_extensions()
+
+bot.run('YOUR_TOKEN_HERE')
